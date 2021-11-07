ab21d7e26709d11869168f0b82b5d2e72399f293
cangokceaslan <cangokceaslanx@gmail.com>
2021-11-07 22:03:56 +0300

    Functions corrected

--- src/modules/coordinates.js
+++ src/modules/coordinates.js
@@ -9,7 +9,7 @@ export const generateRandomLong = () => {
 }
 // LATITUDE -90 to +90
 export const generateRandomLat = () => {
-    var num = (Math.random() * 180).toFixed(3);
+    var num = (Math.random() * 90).toFixed(3);
     var posorneg = Math.floor(Math.random());
     if (posorneg == 0) {
         num = num * -1;
