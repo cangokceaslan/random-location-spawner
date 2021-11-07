5237645d94d56945468b164f45c53fb892fc44ec
cangokceaslan <cangokceaslanx@gmail.com>
2021-11-07 21:55:08 +0300

    updated

--- build/modules/coordinates.js
+++ build/modules/coordinates.js
@@ -9,6 +9,11 @@ exports.generateRandomLong = exports.generateRandomLat = void 0;
 var generateRandomLong = function generateRandomLong() {
   var num = (Math.random() * 180).toFixed(3);
   var posorneg = Math.floor(Math.random());
+
+  if (posorneg == 0) {
+    num = num * -1;
+  }
+
   return num;
 }; // LATITUDE -90 to +90
 
@@ -18,6 +23,11 @@ exports.generateRandomLong = generateRandomLong;
 var generateRandomLat = function generateRandomLat() {
   var num = (Math.random() * 90).toFixed(3);
   var posorneg = Math.floor(Math.random());
+
+  if (posorneg == 0) {
+    num = num * -1;
+  }
+
   return num;
 };
 
