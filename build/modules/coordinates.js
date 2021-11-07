6e2f422b9dfc42fc040da19e30fb76424586f62f
cangokceaslan <cangokceaslanx@gmail.com>
2021-11-07 21:54:09 +0300

    Functions corrected

--- build/modules/coordinates.js
+++ build/modules/coordinates.js
@@ -9,11 +9,6 @@ exports.generateRandomLong = exports.generateRandomLat = void 0;
 var generateRandomLong = function generateRandomLong() {
   var num = (Math.random() * 180).toFixed(3);
   var posorneg = Math.floor(Math.random());
-
-  if (posorneg == 0) {
-    num = num * -1;
-  }
-
   return num;
 }; // LATITUDE -90 to +90
 
@@ -23,11 +18,6 @@ exports.generateRandomLong = generateRandomLong;
 var generateRandomLat = function generateRandomLat() {
   var num = (Math.random() * 90).toFixed(3);
   var posorneg = Math.floor(Math.random());
-
-  if (posorneg == 0) {
-    num = num * -1;
-  }
-
   return num;
 };
 
--- src/modules/coordinates.js
+++ src/modules/coordinates.js
@@ -1,7 +1,6 @@
 // LONGITUDE -180 to + 180
-
-export const generateRandomLong = () =>  {
-    var num = (Math.random()*180).toFixed(3);
+export const generateRandomLong = () => {
+    var num = (Math.random() * 180).toFixed(3);
     var posorneg = Math.floor(Math.random());
     if (posorneg == 0) {
         num = num * -1;
@@ -9,9 +8,8 @@ export const generateRandomLong = () =>  {
     return num;
 }
 // LATITUDE -90 to +90
-
 export const generateRandomLat = () => {
-    var num = (Math.random()*90).toFixed(3);
+    var num = (Math.random() * 90).toFixed(3);
     var posorneg = Math.floor(Math.random());
     if (posorneg == 0) {
         num = num * -1;
